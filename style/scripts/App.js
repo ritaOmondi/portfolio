fe616e17d8fd65d71070947dcb56ebff739bab2e
ritaOmondi <cherylritaomondi@gmail.com>
2024-06-16 22:52:38 +0300

    navigation bar

---
+++ style/scripts/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SearchBar } from './scripts/searchBar'; // Import the SearchBar component
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar /> {/* Use the SearchBar component */}
+    </div>
+  );
+}
+
+export default App;
